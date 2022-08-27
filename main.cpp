4e29da7f288c38e4f47ec46d98edfe11b3e5a14a
Rutesh Shah <96240108+ruteshdak@users.noreply.github.com>
2022-08-27 14:43:28 +0000

    contest

--- main.cpp
+++ main.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
 using namespace std;
-int main(){
+int main() {
   int T;
-  cin>>T;
-  while(T--){
-    
-    int n,count=1;
-    cin>>n;
-    for(int i=1;i<=n;i++){
-     count = count*i;
+  cin >> T;
+  while (T--) {
+    int n, count = 0,co=0;
+    cin >> n;
+
+    string arr[100];
+
+    for (int i = 0; i < n; i++) {
+      cin >> arr[i];
     }
-    cout<<count;
+    for (int i = 0; i < n; i++) {
+      if (arr[i] == "START38") {
+        count++;
+
+      } else if(arr[i]=="LTIME108") {
+        
+        co++;
+      }
+    }
+    cout << count <<" "<< co;
   }
 }
