bb293aec0b2626803b10cd4467bf8183c5c580be
Rutesh Shah <96240108+ruteshdak@users.noreply.github.com>
2022-08-27 15:21:29 +0000

    last number

--- main.cpp
+++ main.cpp
@@ -1,26 +1,22 @@
 #include <iostream>
 using namespace std;
-int main() {
+int main(){
   int T;
-  cin >> T;
-  while (T--) {
-    int n, count = 0,co=0;
-    cin >> n;
-
-    string arr[100];
-
-    for (int i = 0; i < n; i++) {
-      cin >> arr[i];
-    }
-    for (int i = 0; i < n; i++) {
-      if (arr[i] == "START38") {
+  cin>>T;
+  while(T--){
+    int arr[100000];
+    int N,L,R,count=0;
+    cin>>N;
+    cin>>R;
+    // cout<<L/10<<endl;
+    // cout<<L%10<<endl;
+    for(int i=N;i<=R;i++){
+      if(i%10==2 || i%10==3 || i%10==9){
         count++;
-
-      } else if(arr[i]=="LTIME108") {
-        
-        co++;
       }
+      
     }
-    cout << count <<" "<< co;
+    cout<<count;
+    
   }
 }
