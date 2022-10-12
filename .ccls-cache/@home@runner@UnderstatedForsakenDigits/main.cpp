42653aedba313f24df61e1aa792d732a76446803
Rutesh Shah <96240108+ruteshdak@users.noreply.github.com>
2022-10-12 05:38:05 +0000

    b

--- .ccls-cache/@home@runner@UnderstatedForsakenDigits/main.cpp
+++ .ccls-cache/@home@runner@UnderstatedForsakenDigits/main.cpp
@@ -7,25 +7,28 @@ int main() {
   int T,arr[7],count=0,ccount=0;
   cin >> T;
   while (T--) {
-    int n;
-    for (int i = 0; i < 7; i++) {
-      cin >> arr[i];
-      
+    int a,b;
+    cin>>a>>b;
+    int n = a;
+    int c = a+b;
+    int d = 500-a*2;
+    int e = 1000 - c*4;
+    int z = d+e;
+    //cout<<z;
+    int s = b;
+    int p = b+a;
+    int q = 1000 - b*4;
+    int r = 500 - p*2;
+    int f = q+r;
+    // cout<<f;
+    if(f==z){
+      cout<<f;
     }
-    for(int i=0;i<7;i++){
-      if(arr[i]==1){
-        count++;
-      }
-      else if(arr[i]==0){
-        ccount++;
-      }
-      }
-      if(count>ccount){
-        cout<<"YES";
-      }
-      else{
-        cout<<"NO";
-      }
-    
+    else if(z>f){
+      cout<<z;
+    }
+    else {
+      cout<<f;
+    }
   }
 }
